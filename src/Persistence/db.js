4cb421090a5166420272d42a80360ca648dfd813
Gabriel Augusto Lemos Silva <gabriel11475@gmail.com>
2023-02-19 19:19:42 -0300

    rf1 fim

--- src/Persistence/db.js
+++ src/Persistence/db.js
@@ -1,3 +1,4 @@
+
 const sequelize = require('sequelize'); //pede sequelize
 const csequelize = new sequelize('banhoetosafacil','root', '123',{  //loga no sequelize
     dialect: 'mysql', host: 'localhost', port: 3306     //usa a configuraão
