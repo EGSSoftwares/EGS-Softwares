985b7bed659b3fa87e2b939633e6d92bc9ce1be8
Gabriel Augusto Lemos Silva <gabriel11475@gmail.com>
2023-02-19 17:20:47 -0300

    closes #24, create db access point

---
+++ src/Persistence/db.js
@@ -0,0 +1,7 @@
+const sequelize = require('sequelize'); //pede sequelize
+const csequelize = new sequelize('banhoetosafacil','root', '123',{  //loga no sequelize
+    dialect: 'mysql', host: 'localhost', port: 3306     //usa a configuraão
+});
+
+module.exports = csequelize;
+
