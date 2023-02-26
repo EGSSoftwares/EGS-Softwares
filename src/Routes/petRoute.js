2e09c438b82c1479d1f29a35b9d11cce1ed470cd
Gabriel Augusto Lemos Silva <gabriel11475@gmail.com>
2023-02-26 19:50:47 -0300

    updated petRoute.js, updated atualizar_pet.ejs, updated atualizar_pet_pagina_busca, updated excluir_pet., created excluir_pet22, create dinformacoes_pet2

--- src/Routes/petRoute.js
+++ src/Routes/petRoute.js
@@ -1,18 +1,17 @@
 const express = require('express');
-const petcrtl = require("../Controller/petController");
+const petcrtl = require("../Controller/PetController");
 
 
 const router=express.Router();
 
 router.get('/style.css', petcrtl.sendcss)
 router.get('/imagens/logo.jpg', petcrtl.sendlogo)
-// CLIENTES
-router.get('/view/tela_sucesso.html', petcrtl.sendTelaSucesso)
+// pets rroutes
+//router.get('/view/tela_sucesso.html', petcrtl.sendTelaSucesso)
 
-router.get("/view/tela_erro.html", petcrtl.sendTelaErro)
+//router.get("/view/tela_erro.html", petcrtl.sendTelaErro)
 
-router.get('/view/controle_pet.html', petcrtl.sendTelaPet)
-router.get('/', clientecrtl.sendTelaCrtlCliente)
+router.get('/', petcrtl.sendTelacrtlPet)
 //router.post('/view/controle_cliente.html', clientecrtl.sendTelaCrtlCliente)
 
 router.get("/cadastrar_pet.html", petcrtl.sendTelaPet)
@@ -21,18 +20,18 @@ router.post('/addpet', petcrtl.addPet)
 
 router.get('/excluir_pet.html', petcrtl.sendTelaExcluir)
 
-router.post('/excluipetePorCpf', petcrtl.sendTelaExcluir2)
+router.post('/excluipetPorCpf', petcrtl.sendTelaExcluir2)
 
 router.post('/deletePet', petcrtl.deletePet)  
 
 router.get('/pagina_buscar_pet.html', petcrtl.sendTelaBuscarPet)
 
-router.post('/exibirPet', petcrtl.mostrarClientes)
+router.post('/exibirPet', petcrtl.exibirPet)
 
 
 router.get('/solicitar_cpf_e_nome.html', petcrtl.solicitarCpfENome)
 
-router.post('/atualizar_pet.ejs', pettecrtl.formattPet)
+router.post('/atualizar_pet.ejs', petcrtl.formattPet)
 
 router.post('/attPet', petcrtl.attPet )
 
