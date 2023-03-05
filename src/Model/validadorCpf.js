a9edcd5b0327c38d0a8c825387f1dcf39949fbde
Gabriel Augusto Lemos Silva <gabriel11475@gmail.com>
2023-03-05 11:05:32 -0300

    created validadorCpf

---
+++ src/Model/validadorCpf.js
@@ -0,0 +1,29 @@
+function validaCpf( cpf){
+    if(cpf!="" && cpf!=null && cpf!=undefined && cpf.length==11){
+        let x= 0;
+        let i, j;
+        for(i=0, j= 10 ; i<9; i++, j-- ){
+            x+=Number(cpf.charAt(i))*j;
+        }
+        let resto = (x*10)%11;
+        if(resto == 10){
+            resto=0;
+        }
+        sentinela = (Number(cpf.charAt(9))== resto);
+        if(sentinela){
+            x=0;
+            for(let i=0, j=11; i<10; i++, j-- ){
+                x+=Number(cpf.charAt(i))*j;
+            }
+            resto = (x*10)%11;
+            if(resto==10){
+                resto=0;
+            }
+            return resto == cpf.charAt(10);
+        }
+    }
+    return false;
+}
+
+//console.log(validaCpf("35756629327"));
+//console.log(validaCpf("123"));
