3d82b8f264f5a70a3cacd271f95e6ebbea7c608b
Gabriel Augusto Lemos Silva <gabriel11475@gmail.com>
2023-03-07 12:48:31 -0300

    updated some files

--- src/Testes/servicecliente.test.js
+++ src/Testes/servicecliente.test.js
@@ -11,3 +11,56 @@ test('testa campos validos', ()=>{
     }
     expect(service.addCliente(result)).toBe(true);
 });
+
+test('testa campos invalidos, cpf errado', ()=>{
+    const result = {
+        nomePessoa: "Teste",
+        tel: "123456789",
+        bairro: "Dona odete",
+        num: 25,
+        rua:"maria da gloria",
+        cid: "Lavras",
+        CPF: "12101178633S"
+    }
+    expect(service.addCliente(result)).toBe(false);
+});
+
+test('testa campos invalidos', ()=>{
+    const result = {
+        nomePessoa: "",
+        tel: "",
+        bairro: "",
+        num: 25,
+        rua:"maria da gloria",
+        cid: "Lavras",
+        CPF: "12101178633S"
+    }
+    expect(service.addCliente(result)).toBe(false);
+    
+});
+
+test('testa campos invalidos 2', ()=>{
+    const result = {
+        nomePessoa: "",
+        tel: "",
+        bairro: "",
+        num: 25,
+        rua:"maria da gloria",
+        cid: "Lavras",
+        CPF: null
+    }
+    expect(service.addCliente(result)).toBe(false);
+});
+
+test('testa campos invalidos, cpf errado', ()=>{
+    const result = {
+        nomePessoa: "Teste",
+        tel: "123456789",
+        bairro: "Dona odete",
+        num: -25,
+        rua:"maria da gloria",
+        cid: "Lavras",
+        CPF: "12101178605"
+    }
+    expect(service.addCliente(result)).toBe(false);
+});
